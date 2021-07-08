14a1f02ef93967200be4ec8ce9c579ecc6aa602f
joewinter <joewinter1999@yahoo.co.uk>
2021-07-08 20:07:12 +0100

    Scraping msw table

--- Scrape.py
+++ Scrape.py
@@ -1,9 +1,14 @@
 import requests
-from bs4 import BeautifulSoup                                       
-URL = 'https://magicseaweed.com/St-Andrews-East-Surf-Report/38/'    
-response = requests.get(URL)                                        
-print(response)                                                     
-page = BeautifulSoup(response.text, 'html.parser')
-MSWheights = page.find_all('h4')
-print(page.title.string)
-print(MSWheights)
+from bs4 import BeautifulSoup                                      
+URL = 'https://magicseaweed.com/St-Andrews-East-Surf-Report/38/'  
+response = requests.get(URL)                                       
+page = BeautifulSoup(response.content, 'html.parser')
+table = page.find(id='msw-js-fc')
+text = table.find_all('td', class_='text-center background-gray-lighter')
+heights = []
+for elem in text:
+    if elem.find('h4',class_="nomargin font-sans-serif heavy") != None:
+        heights.append(elem.find('h4', class_="nomargin font-sans-serif heavy"))
+for elem in heights: 
+    print(elem.text, end='\n'*2)
+
